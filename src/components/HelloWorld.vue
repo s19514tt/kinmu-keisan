aab7e1bf72c04f59ef42b009a01c6b0460dc9641
s19514tt <s19514tt@sfc.keio.ac.jp>
2021-10-20 01:15:57 +0900

    いろいろ

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,10 @@
           append-outer-icon="mdi-reload"
           @click:append-outer="reloadBefore"
         ></v-text-field>
-        <v-btn color="primary" @click="startWorking">勤務開始</v-btn>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="startWorking">勤務開始</v-btn>
+        </v-card-actions>
       </div>
       <div v-else>
         <div>勤務開始:{{ startDateSet }}</div>
@@ -19,14 +22,18 @@
           @click:append-outer="reloadFinish"
         ></v-text-field>
         <v-text-field label="休憩時間(分)" v-model="kyukei" type="number"></v-text-field>
-        <v-btn color="error" @click="cancel">勤務記録取り消し</v-btn>
-        <v-btn color="primary" @click="finishWorking">勤務終了</v-btn>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="error" @click="cancel">勤務記録取り消し</v-btn>
+          <v-btn color="primary" @click="finishWorking">勤務終了</v-btn>
+        </v-card-actions>
       </div>
     </v-card>
     <v-card class="ma-4 pa-4">
       <v-date-picker v-model="bDate"></v-date-picker>
       <v-date-picker v-model="aDate"></v-date-picker>
       <div>{{ chingin }}</div>
+      <div style="white-space: break-spaces;" v-text="chinginlist"></div>
       <div>
         <v-btn @click="calChingin" color="primary">賃金計算</v-btn>
       </div>
@@ -55,7 +62,8 @@ export default Vue.extend({
       finish: string;
       kyukei: number;
     }[],
-    chingin: '未計算'
+    chingin: '未計算',
+    chinginlist: ''
   }),
   mounted: async function () {
     this.aDate = this.formatDate(new Date())
@@ -72,16 +80,22 @@ export default Vue.extend({
       localStorage.setItem('start', this.startDate)
       this.startDateSet = localStorage.getItem('start')
       this.beforeStart = !(localStorage.getItem('start'))
+      this.reloadFinish()
     },
     finishWorking: async function () {
-      await axios.post('http://192.168.1.30:3232/', {
-        start: localStorage.getItem('start'),
-        finish: this.finishDate,
-        kyukei: this.kyukei
-      })
-      localStorage.removeItem('start')
-      this.startDateSet = localStorage.getItem('start')
-      this.beforeStart = !(localStorage.getItem('start'))
+      try {
+        await axios.post('http://192.168.1.30:3232/', {
+          start: localStorage.getItem('start'),
+          finish: this.finishDate,
+          kyukei: this.kyukei
+        })
+        localStorage.removeItem('start')
+        this.startDateSet = localStorage.getItem('start')
+        this.beforeStart = !(localStorage.getItem('start'))
+        alert('勤務時間を登録しました')
+      } catch (e) {
+        alert(`エラー ${e}`)
+      }
     },
     calChingin: async function () {
       const data = await axios.get<{
@@ -93,15 +107,18 @@ export default Vue.extend({
       const filteredWorkData = this.data.filter((item) => new Date(item.start).getTime() >= new Date(this.bDate).getTime() - 32400000 && new Date(item.start).getTime() <= new Date(this.aDate).getTime() + 86400000 - 32400000)
       if (filteredWorkData.length !== 0) {
         const d = filteredWorkData.map((item) => (new Date(item.finish).getTime() - new Date(item.start).getTime()) / 60000 - item.kyukei).reduce((a, b) => a + b)
+        this.chinginlist = filteredWorkData.map((item) => `勤務開始:${new Date(item.start).toLocaleString()} 勤務終了:${new Date(item.finish).toLocaleString()} 休憩:${item.kyukei}分 勤務分数:${(new Date(item.finish).getTime() - new Date(item.start).getTime()) / 60000 - item.kyukei}分`).join('\n')
         this.chingin = `勤務時間 ${d}分(${Math.floor(d / 60)}時間${d % 60}分) 給料 ${d * 33.3333333333333333}円`
       } else {
         this.chingin = '対象期間のデータなし'
       }
     },
     cancel: function () {
-      localStorage.removeItem('start')
-      this.startDateSet = localStorage.getItem('start')
-      this.beforeStart = !(localStorage.getItem('start'))
+      if (window.confirm("本当に取り消しますか？")) {
+        localStorage.removeItem('start')
+        this.startDateSet = localStorage.getItem('start')
+        this.beforeStart = !(localStorage.getItem('start'))
+      }
     },
     formatDate: function (date: Date) {
       var y = date.getFullYear();
